6f4c30576b1cd3d051802ff83d19d699d8b52583
Faaizz <fr33ziey@gmail.com>
2023-06-04 23:44:46 +0200

    feat/add hello world

--- infrastructure/terraform/ecs/outputs.tf
+++ infrastructure/terraform/ecs/outputs.tf
@@ -2,3 +2,8 @@ output "api_url" {
   description = "The URL of the API"
   value       = "http://${module.ecs.alb_dns_name}/api/v1"
 }
+
+output "view_url" {
+  description = "The URL of the view page"
+  value       = "http://${module.ecs.alb_dns_name}/view"
+}
