9cc27d549ea4bb3272e9dbfac4ff897587acc4c2
Faaizz <fr33ziey@gmail.com>
2023-06-04 18:46:55 +0200

    fix/minor fix

--- infrastructure/terraform/modules/vpc/variables.tf
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -8,7 +8,7 @@ variable "cidr" {
   default     = "10.0.0.0/16"
   validation {
     condition     = can(regex("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+/[0-9]+$", var.cidr))
-    error_message = "CIDR block must be in the form x.x.x.x/x"
+    error_message = "CIDR block must be in the form x.x.x.x/x."
   }
 }
 variable "container_port" {
