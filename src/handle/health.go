fe029d830e6134dbfce0a2b90368f94ac8337983
Faaizz <fr33ziey@gmail.com>
2023-06-06 11:32:29 +0200

    fix/return codes

--- src/handle/health.go
+++ src/handle/health.go
@@ -1,12 +1,20 @@
 package handle
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type Status struct {
+	Status string `json:"status"`
+}
+
 func Health(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := Status{"OK"}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w = addHeaders(w)
+	json.NewEncoder(w).Encode(s)
 }
